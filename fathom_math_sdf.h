7410ef535b63fffa88b78a591bae53cc389c3c12
nickscha <nick@scharnbeck.de>
2026-02-23 10:02:07 +0100

    [SDF] inline exact sdf functions

--- fathom_math_sdf.h
+++ fathom_math_sdf.h
@@ -7,25 +7,23 @@
  * # [SECTION] Signed Distance Functions
  * #############################################################################
  */
-FATHOM_API f32 fathom_sdf_sphere(fathom_vec3 pos, f32 radius)
+FATHOM_API FATHOM_INLINE f32 fathom_sdf_sphere(fathom_vec3 pos, f32 radius)
 {
     return fathom_vec3_length(pos) - radius;
 }
 
-FATHOM_API f32 fathom_sdf_box(fathom_vec3 pos, fathom_vec3 base)
+FATHOM_API FATHOM_INLINE f32 fathom_sdf_box(fathom_vec3 pos, fathom_vec3 base)
 {
     fathom_vec3 q = fathom_vec3_sub(fathom_vec3_abs(pos), base);
 
     return fathom_vec3_length(fathom_vec3_maxf(q, 0.0f)) + fathom_minf(fathom_maxf(q.x, fathom_maxf(q.y, q.z)), 0.0f);
 }
 
-/*
-FATHOM_API f32 fathom_sdf_box_rounded(fathom_vec3 pos, fathom_vec3 base, f32 radius)
+FATHOM_API FATHOM_INLINE f32 fathom_sdf_box_rounded(fathom_vec3 pos, fathom_vec3 base, f32 radius)
 {
     fathom_vec3 q = fathom_vec3_addf(fathom_vec3_sub(fathom_vec3_abs(pos), base), radius);
 
     return fathom_vec3_length(fathom_vec3_maxf(q, 0.0f)) + fathom_minf(fathom_maxf(q.x, fathom_maxf(q.y, q.z)), 0.0f) - radius;
 }
-*/
 
 #endif /* FATHOM_MATH_SDF_H */
